c6b0e865786b3412a6806794b3e18e0549ef8187
xzgao <xz.gao@connect.ust.hk>
2023-10-17 09:28:51 +0000

    version detect

--- src/CuTropicalGEMM.jl
+++ src/CuTropicalGEMM.jl
@@ -3,6 +3,12 @@ module CuTropicalGEMM
 using CUDA, TropicalNumbers, LinearAlgebra, TropicalGemmC_jll 
 export matmul!
 
+function __init__()
+    @assert CUDA.driver_version() >= v"11.4" "Error: CUDA.driver_version < v11.4"
+    @assert CUDA.driver_version() <= v"12.1" "Error: CUDA.driver_version > v12.1"
+    return nothing
+end
+
 const Symbol_FP32 = (:FP32, "FP32")
 const Symbol_FP64 = (:FP64, "FP64")
 const Symbol_INT32 = (:INT32, "INT32")
